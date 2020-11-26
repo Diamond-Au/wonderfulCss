fb325287671ca5708b9115404ad40dd327ec1309
Diamond-Au <497350746@qq.com>
2020-11-26 22:05:29 +0800

    fix : bug

--- src/ios-switch/index.jsx
+++ src/ios-switch/index.jsx
@@ -1,7 +1,5 @@
-function App(){
-  return (
-    <div>this is a test</div>
-  )
+function App() {
+  return <div>this is a test</div>;
 }
 
-ReactDOM.render(App(), document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector("#root"));
